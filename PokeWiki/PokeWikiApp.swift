350236cedacdf61a0ad9d208d79279a68a6671ec
zhydani <zhydani@gmail.com>
2023-04-30 17:04:58 -0300

    mudando escopo do projeto

---
+++ PokeWiki/PokeWikiApp.swift
@@ -0,0 +1,18 @@
+//
+//  PokeWikiApp.swift
+//  PokeWiki
+//
+//  Created by Zhydani Reis benigno  on 30/04/23.
+//
+
+import Foundation
+import SwiftUI
+
+@main
+struct MVVMPokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
