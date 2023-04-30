18531a85aab5056c6dbf8681112c0a1dcef93db1
zhydani <zhydani@gmail.com>
2023-04-30 17:12:26 -0300

    adicionando contetudo do content view tela principal

--- PokeWiki/Views/ContentView.swift
+++ PokeWiki/Views/ContentView.swift
@@ -8,10 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var vm = ViewModel()
+    
+    private let adaptiveColumns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: adaptiveColumns, spacing: 10) {
+                    ForEach(vm.filteredPokemon) { pokemon in
+                        NavigationLink(destination: PokemonDetailView(pokemon: pokemon)
+                        ) {
+                            PokemonView(pokemon: pokemon)
+                        }
+                    }
+                }
+                .animation(.easeInOut(duration: 0.3), value: vm.filteredPokemon.count)
+                .navigationTitle("PokemonUI")
+                .navigationBarTitleDisplayMode(.inline)
+            }
+            .searchable(text: $vm.searchText)
+        }
+        .environmentObject(vm)
+    }}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
