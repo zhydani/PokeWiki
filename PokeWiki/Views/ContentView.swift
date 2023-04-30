f0df6441fff5a066bd070fa9cad2a71d263dd046
zhydani <zhydani@gmail.com>
2023-04-30 18:10:01 -0300

    aplicativo funcionando

---
+++ PokeWiki/Views/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  PokeWiki
+//
+//  Created by Zhydani Reis benigno  on 30/04/23.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    @StateObject var vm = ViewModel()
+    
+    private let adaptiveColumns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: adaptiveColumns, spacing: 10) {
+                    ForEach(vm.filteredPokemon) { pokemon in
+                        NavigationLink(destination: PokemonDetailView(pokemon: pokemon)
+                        ) {
+                            PokemonView(pokemon: pokemon)
+                        }
+                    }
+                }
+                .animation(.easeInOut(duration: 0.3), value: vm.filteredPokemon.count)
+                .navigationTitle("PokemonUI")
+                .navigationBarTitleDisplayMode(.inline)
+            }
+            .searchable(text: $vm.searchText)
+        }
+        .environmentObject(vm)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
